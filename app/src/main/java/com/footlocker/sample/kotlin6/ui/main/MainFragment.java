12b31b5cdb06006de80afbbad227f61e0091411d
James Harmon <james.harmon@footlocker.com>
2019-03-24 14:40:17 -0500

    initial commit

---
+++ app/src/main/java/com/footlocker/sample/kotlin6/ui/main/MainFragment.java
@@ -0,0 +1,52 @@
+package com.footlocker.sample.kotlin6.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.footlocker.sample.kotlin6.R;
+
+public class MainFragment extends Fragment {
+
+    int counter = 0;
+
+    @BindView(R.id.counter)
+    TextView textView;
+    @BindView(R.id.button)
+    Button button;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        textView.setText(Integer.toString(counter));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                textView.setText(Integer.toString(counter));
+            }
+        });
+
+        return view;
+    }
+
+}
