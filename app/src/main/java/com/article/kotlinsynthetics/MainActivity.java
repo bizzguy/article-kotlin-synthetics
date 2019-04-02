28a5c9dd7c332079227969d516d49b4dedd326af
bizzguy <jamesharmon@gmail.com>
2019-04-02 09:40:50 -0500

    Update MainActivity.java

--- app/src/main/java/com/article/kotlinsynthetics/MainActivity.java
+++ app/src/main/java/com/article/kotlinsynthetics/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        getSupportActionBar().setTitle("Kotlin Talk 2");
+        getSupportActionBar().setTitle("Kotlin Talk");
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
